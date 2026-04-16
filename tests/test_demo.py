a88b9322992c4c77a56365a56e43016db11e7789
ttolmacheva-qa <tattolmacheva89@gmail.com>
2026-04-16 11:19:24 +0500

    test: add second form test with special chars

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,10 +1,17 @@
-
 from pages.demo_page import DemoPage
 
-def test_form_submit(page):
+def test_form_submit_valid(page):
     demo = DemoPage(page)
     demo.open()
     demo.fill_form("Tolmacheva QA", "ttolmacheva@test.com")
     output = demo.get_output()
     assert "Tolmacheva QA" in output
     assert "ttolmacheva@test.com" in output
+
+def test_form_submit_with_special_chars(page):
+    demo = DemoPage(page)
+    demo.open()
+    demo.fill_form("QA Тест", "qa+test@example.com")
+    output = demo.get_output()
+    assert "QA Тест" in output
+    assert "qa+test@example.com" in output
